9a743d49bb7bbdec84bc06e5b043c327d33f8c93
Md Tohidur Rahman Siam <trsiam3468@gmail.com>
2025-08-30 23:43:01 +0600

    Add files via upload

---
+++ Roman to Integer.py
@@ -0,0 +1,77 @@
+#https://leetcode.com/problems/roman-to-integer/description/
+
+
+
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+        
+
+
+# I- 1
+# II- 2
+# III-3
+# IV-4
+# V-5
+# VI-6
+# VII-7
+# VIII-8
+# IX-9
+# X-10
+# XI-11
+# XII-12
+# XIII-13
+# XIV-14
+# XV-15
+# XVI-16
+# XVII-17
+# XVIII-18
+# XIX-19
+# XX-20
+# XXI-21
+roman=str(input())
+count=0
+I=1
+V=5
+X=10
+L=50
+C=100
+D=500
+H=1000
+roman_Values={"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "H":1000}
+
+for i in range(len(roman)):
+    
+
+
+
+
+
+
+
+
+
+
+
+print(roman_Values["I"])
+
+# print(roman)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
