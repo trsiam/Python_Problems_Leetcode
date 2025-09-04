89096d553c78c501f419ad1a2ea8716c0bc2f684
Md Tohidur Rahman Siam <trsiam3468@gmail.com>
2025-09-04 14:55:57 +0600

    Add files via upload

---
+++ Search Insert Position.py
@@ -0,0 +1,40 @@
+#https://leetcode.com/problems/search-insert-position/description/
+
+
+
+# nums=[1,3,5,6]
+# target=2
+# flag=False
+# k=0
+# for i in range(len(nums)):
+#     if nums[i] == target:
+#         print(i)
+#         break
+#     elif(nums[i] < target <= nums[k]):
+#         print(k)
+#         break
+#     elif(nums[i]<target):
+#         flag=True
+#     k+=1
+# if flag == True:
+#     print(k)
+# if nums[0]>target:
+#     print(0)
+
+
+
+
+
+# class Solution:
+#  def searchInsert(self, nums: List[int], target: int) -> int:
+#   nums.append(target)
+#   nums.sort()
+#   return nums.index(target)
+
+
+
+
+
+
+
+
