d0c52c11441f6f9db76b99e47f53fee7c5b10ecb
Md Tohidur Rahman Siam <trsiam3468@gmail.com>
2025-09-04 21:08:28 +0600

    Add files via upload

---
+++ Length of Last Word.py
@@ -0,0 +1,28 @@
+#https://leetcode.com/problems/length-of-last-word/description/
+
+
+
+# s="   fly me   to   the moon  "
+# list1=list(s.split())
+# print(len(list1[-1]))
+# v1=len(s)
+# print(v1)
+
+
+
+s="   fly me   to   the moon  "
+list1=list(s.strip())
+v1=len(s)
+print(list1)
+
+
+
+
+
+
+
+
+
+
+
+
